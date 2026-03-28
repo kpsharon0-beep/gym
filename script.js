a305bf0f87119b9256de027041fb031c692e2738
kpsharon0-beep <kpsharon0@gmail.com>
2026-03-28 19:40:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ function sendWhatsApp(){
 Name: ${name}
 Phone: ${phone}`;
 
-  let url="https://wa.me/919999999999?text="+encodeURIComponent(msg);
+  let url="https://wa.me/918129638606?text="+encodeURIComponent(msg);
   window.open(url,"_blank");
 }
